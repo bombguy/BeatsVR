345708534e5518912824828949de8637b951cb91
Your Name <you@example.com>
2017-01-14 07:47:25 -0800

    first game alpha alpha alpafaca

---
+++ Assets/Scripts/BeatsVRController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatsVRController : MonoBehaviour {
+
+
+	private SteamVR_TrackedObject trackedObject;
+	private SteamVR_Controller.Device device;
+
+	Transform contactTarget = null;
+
+	// Use this for initialization
+	void Start () {
+		trackedObject = GetComponent<SteamVR_TrackedObject> ();
+		device = SteamVR_Controller.Input ((int)trackedObject.index);
+	}
+
+	void fire (bool line_of_sight, RaycastHit hit) {
+		if (line_of_sight) {
+			device.TriggerHapticPulse (3000);
+			Debug.Log ("Target HIT");
+			Destroy (hit.transform.gameObject);
+		}
+	}
+
+	void checkAim(bool line_of_sight, RaycastHit hit) {
+		if (!line_of_sight || (contactTarget && contactTarget != hit.transform))
+		{
+			if (contactTarget != null)
+				contactTarget.GetComponent<TargetBehavior> ().isTarget = false;
+			contactTarget = null;
+		}
+		if (line_of_sight) {
+			hit.transform.GetComponent<TargetBehavior> ().isTarget = true;
+			contactTarget = hit.transform;
+		}
+	}
+
+	void checkCollide(bool bHit, RaycastHit hit)
+	{
+		//reset if beam not hitting or hitting new target
+
+		/*if (!bHit || (contactTarget && contactTarget != hit.transform))
+		{
+			if (contactTarget != null)
+				changeColor (contactTarget.transform.gameObject, Color.black);
+			contactTarget = null;
+		}*/
+
+		//check if beam has hit a new target
+		if (bHit)
+		{
+			device.TriggerHapticPulse (3000);
+			DestroyObject (hit.transform.gameObject);
+			//changeColor (hit.transform.gameObject, Color.green);
+			//contactTarget = hit.transform;
+		}
+	}
+
+	void changeColor (GameObject obj, Color color) {
+		obj.GetComponent<Renderer> ().material.color = color;
+	}
+
+	void Update() {
+		Ray raycast = new Ray (transform.position, transform.forward);
+		RaycastHit hitObject;
+		bool rayHit = Physics.Raycast (raycast, out hitObject);
+
+		checkAim (rayHit, hitObject);
+		if (device.GetPressDown (SteamVR_Controller.ButtonMask.Trigger)) {
+			fire (rayHit, hitObject);
+		}
+
+
+	}
+
+} 
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject target_prefab;
+	public double spawn_rate;
+
+	private double timer;
+
+	// Use this for initialization
+	void Start () {
+		timer = 0;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer > spawn_rate) {
+			timer = 0;
+
+			Vector3 pos = new Vector3 (Random.Range (0f, 1f), Random.Range (1f, 2f), Random.Range (-0.5f, -1f));
+			Instantiate (target_prefab, pos, Quaternion.identity);
+		}
+	}
+}
