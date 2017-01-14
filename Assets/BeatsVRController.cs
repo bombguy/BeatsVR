0f2d23ce756c09d4bf755af621084555add2ec25
Your Name <you@example.com>
2017-01-14 03:23:38 -0800

    Initial commit

---
+++ Assets/BeatsVRController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatsVRController : MonoBehaviour {
+
+
+	private SteamVR_TrackedObject trackedObject;
+	private SteamVR_Controller.Device device;
+
+	// Use this for initialization
+	void Start () {
+		trackedObject = GetComponent<SteamVR_TrackedObject> ();
+	}
+
+	void Trigger (object sender, ClickedEventArgs e)
+	{
+		Debug.Log ("Trigger Clicked");
+	}
+
+
+	// Update is called once p..........,
+	void Update() {
+		device = SteamVR_Controller.Input ((int)trackedObject.index);
+		if (device.GetAxis().x != 0 || device.GetAxis().y != 0) {
+			//Debug.Log (device.GetAxis().x + " " + device.GetAxis ().y);
+		}
+		if (device.GetPressDown (SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log ("Trigger Press");
+			device.TriggerHapticPulse(3999);
+			Debug.Log (device.GetAxis().x + " " + device.GetAxis ().y);
+		}
+	}
+
+} 
